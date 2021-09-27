d31e4341cf8aafcefabe56a268f0578a554749e3
edpackard <edpackard81@gmail.com>
2021-09-27 17:14:36 +0100

    working on account methods

--- lib/account.rb
+++ lib/account.rb
@@ -1,3 +1,21 @@
+require 'transaction'
+
 class Account
+
+  def initialize(transaction = Transaction)
+    @transaction = transaction
+  end
+
+  def current_balance()
+    return 0
+  end
+
+  def deposit(amount)
+    @transaction.new.deposit(amount)
+  end
+
+  def withdraw(amount)
+    @transaction.new.withdraw(amount)
+  end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,6 +2,27 @@ require 'account'
 
 describe Account do
 
-  it { is_expected.to be_a Account }
+  let(:transaction_class) { double :transaction_class }
+  let(:transaction_instance) { double :transaction_instance }
+
+  subject { Account.new(transaction_class) }
+
+  before(:each) do
+    allow(transaction_class).to receive(:new).and_return(transaction_instance)
+  end
+  
+  it "starts with a balance of zero" do
+    expect(subject.current_balance).to eq(0)
+  end
+
+  it "can call transaction deposit function" do
+    expect(transaction_instance).to receive(:deposit).with(100.00)
+    subject.deposit(100.00)  
+  end
+
+  it "can call transaction withdraw function" do
+    expect(transaction_instance).to receive(:withdraw).with(100.00)
+    subject.withdraw(100.00)
+  end
 
 end
