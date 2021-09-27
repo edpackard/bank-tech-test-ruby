1ca9fa0406075e495a06f06e161e0f3b1619f9f4
edpackard <edpackard81@gmail.com>
2021-09-27 14:13:32 +0100

    update tests for transaction class to use float values

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -3,12 +3,12 @@ require 'transaction'
 describe Transaction do
 
   it 'records a deposit as a credit' do 
-    subject.deposit(10)
-    expect(subject.credit).to eq(10)
+    subject.deposit(10.00)
+    expect(subject.credit).to eq(10.00)
   end
 
   it 'records a withdrawal as a debit' do
-    subject.withdrawal(10)
-    expect(subject.debit).to eq(10)
+    subject.withdrawal(10.00)
+    expect(subject.debit).to eq(10.00)
   end
 end
