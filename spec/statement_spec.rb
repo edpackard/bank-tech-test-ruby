55555bf3fb972a9fa2ca612b870a42eda67f1071
edpackard <edpackard81@gmail.com>
2021-09-29 09:15:09 +0100

    refactoring

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -2,9 +2,9 @@ require 'statement'
 
 describe Statement do
 
-  let(:transaction_1) { double :transaction_instance, credit: 0.0, debit: 10.00, date: Time.new(2021, 9, 1), balance: -10.00 }
-  let(:transaction_2) { double :transaction_instance, credit: 20.00, debit: 0.0, date: Time.new(2021, 9, 27), balance: 10.00 }
-  let(:transaction_3) { double :transaction_instance, credit: 10.00, debit: 0.0, date: Time.new(2021, 9, 28), balance: 20.00 }
+  let(:record_1) { double credit: 0.0, debit: 10.00, date: Time.new(2021, 9, 1), balance: -10.00 }
+  let(:record_2) { double credit: 20.00, debit: 0.0, date: Time.new(2021, 9, 27), balance: 10.00 }
+  let(:record_3) { double credit: 10.00, debit: 0.0, date: Time.new(2021, 9, 28), balance: 20.00 }
 
   it "prints the header" do
     transactions = []
@@ -12,12 +12,12 @@ describe Statement do
   end
 
   it "prints a statement" do
-    expected_output =
-      "28/09/2021 || 10.00 || || 20.00\n"\
+    output_string = "28/09/2021 || 10.00 || || 20.00\n"\
       "27/09/2021 || 20.00 || || 10.00\n"\
       "01/09/2021 || || 10.00 || -10.00\n"
-    transactions = [transaction_1, transaction_2, transaction_3]
-    expect { subject.printer(transactions) }.to output(Statement::HEADING + expected_output).to_stdout
+    expected_output = Statement::HEADING + output_string
+    transactions = [record_1, record_2, record_3]
+    expect { subject.printer(transactions) }.to output(expected_output).to_stdout
   end
 
 end
