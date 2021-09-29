5cf3b12afd71f37e71730cc9b400c1af52d18644
edpackard <edpackard81@gmail.com>
2021-09-29 12:01:47 +0100

    add timecop to feature test

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -1,10 +1,11 @@
 require 'account'
+require 'timecop'
 
 describe 'feature test' do
 
-  time = Time.now
-  date = time.strftime("%d/%m/%Y")
-  
+  time = Time.local(2022, 1, 1)
+  Timecop.freeze(time)
+
   it 'makes several transactions' do
     account = Account.new
     account.deposit(10.00)
@@ -18,17 +19,21 @@ describe 'feature test' do
     account.deposit(10.00)
     account.withdraw(5.00)
     account.deposit(2.50)
+    time = Time.local(2022, 2, 1)
+    Timecop.freeze(time)
     account.deposit(20.00)
     account.withdraw(3.00)
+    time = Time.local(2022, 2, 14)
+    Timecop.freeze(time)
     account.deposit(500.00)
     expected_output =
     "date || credit || debit || balance\n"\
-    "#{date} || 500.00 || || 524.50\n"\
-    "#{date} || || 3.00 || 24.50\n"\
-    "#{date} || 20.00 || || 27.50\n"\
-    "#{date} || 2.50 || || 7.50\n"\
-    "#{date} || || 5.00 || 5.00\n"\
-    "#{date} || 10.00 || || 10.00\n"
+    "14/02/2022 || 500.00 || || 524.50\n"\
+    "01/02/2022 || || 3.00 || 24.50\n"\
+    "01/02/2022 || 20.00 || || 27.50\n"\
+    "01/01/2022 || 2.50 || || 7.50\n"\
+    "01/01/2022 || || 5.00 || 5.00\n"\
+    "01/01/2022 || 10.00 || || 10.00\n"
     expect { account.statement }.to output(expected_output).to_stdout
   end
 
